4109844b80ff8714167ad667c2baa3476be7ee9c
Shaden Shanab <shadenshanab2@gmail.com>
2022-12-07 18:08:14 +0000

    add: base code

--- django_project/settings.py
+++ django_project/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-SECRET_KEY
-SECRET_KEY = "django-insecure-0peo@#x9jur3!h$ryje!$879xww8y1y66jx!%*#ymhg&jkozs2"
+SECRET_KEY = "django-insecure-oop90peo@#x9jur3!h$ryje!$879xww8y1y66jx!%*#ymhg&jkozs2"
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
@@ -31,6 +31,8 @@ INSTALLED_APPS = [
     # Local
     "accounts",
     "pages",
+    # Own
+    "shop",
 ]
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#middleware
